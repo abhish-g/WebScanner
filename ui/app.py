714216386e00d59091b3ed5d726ace13cd5fb83c
abhish-g <abhish.gupta2004@gmail.com>
2026-08-26 00:36:46 +0530

    Prepare app for deployment

--- ui/app.py
+++ ui/app.py
@@ -393,9 +393,10 @@ def scan():
 # ============================================================
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
 
     app.run(
-        host="127.0.0.1",
-        port=8000,
-        debug=True
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
